9d1b1c861e2f1cb7dc53c452f6e158e92ca2a86f
Mudit Sharma <mudit.sharma@codethink.co.uk>
2023-01-29 18:41:55 +0000

    Added code to handle AT commands over serial

---
+++ reva-firmware/Core/Inc/at_cmds.h
@@ -0,0 +1,47 @@
+#ifndef _AT_CMDS_
+#define _AT_CMDS_
+
+#include "main.h"
+#include <string.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include "usb_device.h"
+#include "gpio.h"
+#include "stdio.h"
+#include "usbd_cdc_if.h"
+#include "switch_ctrl.h"
+
+// Enum defining detecting AT cmds. 
+typedef enum
+{
+  AT_ENTER,
+  AT_TEST,
+  AT_INFO,
+  AT_PORTS_DISABLED,
+  AT_PORT_A_SOURCE_B, // A SOURCE
+  AT_PORT_A_SOURCE_C, // A SOURCE
+  AT_PORT_B_SOURCE_A, // B SOURCE
+  AT_PORT_C_SOURCE_A, // C SOURCE
+  AT_PORT_NUMBERS_QUERY,
+  AT_PORT_SEL,
+  AT_PORT_CMD_ERROR,
+  AT_UNKNOWN_CMD,
+} at_cmds_e;
+
+
+/**
+ * @brief Prase input buffer data 
+ * 
+ * @param char *rx_data: receive buffer to pass
+ * @return at_cmd_e: Recognized AT cmd  
+*/
+at_cmds_e parse_input(char *rx_data);
+
+/**
+ * @brief Process received data and respond to recognized 
+ * AT commands.  
+*/
+void process_input(uint8_t *input);
+
+#endif
---
+++ reva-firmware/Core/Src/at_cmds.c
@@ -0,0 +1,187 @@
+#include "at_cmds.h"
+#include "main.h"
+
+const char *FIRMWARE_VERSION = "1.0";
+const char *HARDWARE_VERSION = "Rev-B";
+uint8_t selected_port = AT_PORTS_DISABLED;
+
+at_cmds_e parse_input(char *rx_data)
+{
+    if (strncmp(rx_data, "\r", 1) == 0 || strncmp(rx_data, "\n", 1) == 0)
+    {
+        return AT_ENTER;
+    }
+
+    if (strncmp(rx_data, "AT", 2))
+    {
+        return AT_UNKNOWN_CMD;
+    }
+    else
+    {
+        if (strlen(rx_data) == 3 && (rx_data[2] == '\n' || rx_data[2] == '\r'))
+        {
+            return AT_TEST;
+        }
+        else if (rx_data[2] == '+')
+        {
+            if ((strncmp(&rx_data[3], "GMR\n", 4) == 0) || (strncmp(&rx_data[3], "GMR\r", 4) == 0))
+            {
+                return AT_INFO;
+            }
+            else if (strncmp(&rx_data[3], "PORT", 4) == 0)
+            {
+                if (rx_data[7] == '=')
+                {
+                    if (rx_data[8] == '0')
+                    {
+                        return AT_PORTS_DISABLED;
+                    }
+                    else if (rx_data[8] == '1')
+                    {
+                        return AT_PORT_A_SOURCE_C;
+                    }
+                    else if (rx_data[8] == '2')
+                    {
+                        return AT_PORT_A_SOURCE_B;
+                    }
+                    else if (rx_data[8] == '3')
+                    {
+                        return AT_PORT_C_SOURCE_A;
+                    }
+                    else if (rx_data[8] == '4')
+                    {
+                        return AT_PORT_B_SOURCE_A;
+                    }
+                    else if (rx_data[8] == '?')
+                    {
+                        return AT_PORT_NUMBERS_QUERY;
+                    }
+                    else
+                    {
+                        return AT_PORT_CMD_ERROR;
+                    }
+                }
+                else if (rx_data[7] == '?')
+                {
+                    return AT_PORT_SEL;
+                }
+            }
+            else
+            {
+                return AT_UNKNOWN_CMD;
+            }
+        }
+        else
+        {
+            return AT_UNKNOWN_CMD;
+        }
+    }
+    return AT_UNKNOWN_CMD;
+}
+
+void process_input(uint8_t *input)
+{
+    switch (parse_input((char *)input))
+    {
+    case AT_TEST:
+        printf("\rOK\r\n");
+        local_buffer_flush(input, LOCAL_BUFFER_SIZE);
+        break;
+
+    case AT_INFO:
+        printf("Codethink USB C switch\n\rHW:%s\r\nFW:V%s\r\n", HARDWARE_VERSION, FIRMWARE_VERSION);
+        local_buffer_flush(input, LOCAL_BUFFER_SIZE);
+        break;
+
+    case AT_PORTS_DISABLED:
+        printf("\rOK\r\n");
+        port_switch_control(PORTS_DISABLED, PORTS_OFF);
+        selected_port = AT_PORTS_DISABLED;
+        local_buffer_flush(input, LOCAL_BUFFER_SIZE);
+        break;
+
+    case AT_PORT_A_SOURCE_B:
+        printf("\rOK\r\n");
+        port_switch_control(PORTS_DISABLED, PORTS_OFF);
+        HAL_Delay(500);
+        port_switch_control(PORT_D_EN, PORT_SINK);
+        selected_port = AT_PORT_A_SOURCE_B;
+        local_buffer_flush(input, LOCAL_BUFFER_SIZE);
+        break;
+
+    case AT_PORT_A_SOURCE_C:
+        printf("\rOK\r\n");
+        port_switch_control(PORTS_DISABLED, PORTS_OFF);
+        HAL_Delay(500);
+        port_switch_control(PORT_C_EN, PORT_SINK);
+        selected_port = AT_PORT_A_SOURCE_C;
+        local_buffer_flush(input, LOCAL_BUFFER_SIZE);
+        break;
+
+    case AT_PORT_B_SOURCE_A:
+        printf("\rOK\r\n");
+        port_switch_control(PORTS_DISABLED, PORTS_OFF);
+        HAL_Delay(500);
+        port_switch_control(PORT_D_EN, PORT_SOURCE);
+        selected_port = AT_PORT_B_SOURCE_A;
+        local_buffer_flush(input, LOCAL_BUFFER_SIZE);
+        break;
+
+    case AT_PORT_C_SOURCE_A:
+        printf("\rOK\r\n");
+        port_switch_control(PORTS_DISABLED, PORTS_OFF);
+        HAL_Delay(500);
+        port_switch_control(PORT_C_EN, PORT_SOURCE);
+        selected_port = AT_PORT_C_SOURCE_A;
+        local_buffer_flush(input, LOCAL_BUFFER_SIZE);
+        break;
+
+    case AT_PORT_SEL:
+        if (selected_port == AT_PORT_A_SOURCE_B)
+        {
+            printf("\rA source B sink\r\n");
+        }
+        else if (selected_port == AT_PORT_A_SOURCE_C)
+        {
+            printf("\rA source C sink\r\n");
+        }
+        else if (selected_port == AT_PORT_B_SOURCE_A)
+        {
+            printf("\rB source A sink\r\n");
+        }
+        else if (selected_port == AT_PORT_C_SOURCE_A)
+        {
+            printf("\rC source A sink\r\n");
+        }
+        else
+        {
+            printf("\rPORTS DISABLED\r\n");
+        }
+        local_buffer_flush(input, LOCAL_BUFFER_SIZE);
+        break;
+
+    case AT_PORT_NUMBERS_QUERY:
+        printf("\r2\r\nOK\r\n");
+        local_buffer_flush(input, LOCAL_BUFFER_SIZE);
+        break;
+
+    case AT_PORT_CMD_ERROR:
+        printf("\rERROR: ENTER CORRECT PORT NUMBER\r\n");
+        local_buffer_flush(input, LOCAL_BUFFER_SIZE);
+        break;
+
+    case AT_UNKNOWN_CMD:
+        printf("\rERROR\r\n");
+        port_switch_control(PORTS_DISABLED, PORTS_OFF);
+        local_buffer_flush(input, LOCAL_BUFFER_SIZE);
+        break;
+
+    case AT_ENTER:
+        printf("\r\n");
+        local_buffer_flush(input, LOCAL_BUFFER_SIZE);
+
+    default:
+        local_buffer_flush(input, LOCAL_BUFFER_SIZE);
+        break;
+    }
+}
