1e7ee0ce8d724cf6db59cebbc257702e529cdd07
Mudit Sharma <mudit.sharma@codethink.co.uk>
2023-03-23 10:58:50 +0000

    Added new AT commands for new modes introduced
    Modes include:
    * Bi-directional power switching
    * Only power on for all ports.

--- reva-firmware/usb_switch_code/at_cmds.c
+++ reva-firmware/usb_switch_code/at_cmds.c
@@ -32,6 +32,8 @@
 
 #include "at_cmds.h"
 #include "main.h"
+#include "stm32f1xx_hal.h"
+#include "switch_ctrl.h"
 
 // Delay between USB port switching
 #define DELAY_BW_SWITCH 500 // ms
@@ -43,173 +45,225 @@ uint8_t selected_port = AT_PORTS_DISABLED;
 at_cmds_e parse_input(const char *rx_data)
 {
 
-    if (!strcmp(rx_data, "AT"))
-    {
-        return AT_TEST;
-    }
+  if (!strcmp(rx_data, "AT"))
+  {
+    return AT_TEST;
+  }
 
-    // Does the received string starts with AT?
-    if (strncmp(rx_data, "AT", 2) != 0) // If first 2 char != 'AT'
+  // Does the received string starts with AT?
+  if (strncmp(rx_data, "AT", 2) != 0) // If first 2 char != 'AT'
+  {
+    // return unknown if not
+    return AT_UNKNOWN_CMD;
+  }
+
+  // if first 2 bytes are "AT"
+  else
+  {
+    // does it have a '+' after?
+    if (rx_data[2] == '+')
     {
-        // return unknown if not
+      // is the '+' followed by "GMR"?
+      if ((strncmp(&rx_data[3], "GMR", 3) == 0))
+      {
+        return AT_INFO;
+      }
+      // is the '+' followed by "PORT"?
+      else if (strncmp(&rx_data[3], "PORT", 4) == 0)
+      {
+        // is "PORT" followed by '='?
+        if (rx_data[7] == '=')
+        {
+          if (rx_data[8] == '0')
+          {
+            return AT_PORTS_DISABLED;
+          }
+          else if (rx_data[8] == '1')
+          {
+            return AT_PORT_A_SOURCE_C;
+          }
+          else if (rx_data[8] == '2')
+          {
+            return AT_PORT_A_SOURCE_B;
+          }
+          else if (rx_data[8] == '3')
+          {
+            return AT_PORT_C_SOURCE_A;
+          }
+          else if (rx_data[8] == '4')
+          {
+            return AT_PORT_B_SOURCE_A;
+          }
+          else if (rx_data[8] == '5')
+          {
+            return AT_PORT_A_C_ON;
+          }
+          else if (rx_data[8] == '6')
+          {
+            return AT_PORT_A_B_ON;
+          }
+          else if (rx_data[8] == '7')
+          {
+            return AT_PORT_A_B_C_ON;
+          }
+          else if (rx_data[8] == '?')
+          {
+            return AT_PORT_NUMBERS_QUERY;
+          }
+          else
+          {
+            return AT_PORT_CMD_ERROR;
+          }
+        }
+        else if (rx_data[7] == '?')
+        {
+          return AT_PORT_SEL;
+        }
+      }
+      // if not followed by "PORT" its unknown
+      else
+      {
         return AT_UNKNOWN_CMD;
+      }
     }
-
-    // if first 2 bytes are "AT"
+    // if input string starts with "AT" but it is not followed by anything
     else
     {
-        // does it have a '+' after?
-        if (rx_data[2] == '+')
-        {
-            // is the '+' followed by "GMR"?
-            if ((strncmp(&rx_data[3], "GMR", 3) == 0))
-            {
-                return AT_INFO;
-            }
-            // is the '+' followed by "PORT"?
-            else if (strncmp(&rx_data[3], "PORT", 4) == 0)
-            {
-                // is "PORT" followed by '='?
-                if (rx_data[7] == '=')
-                {
-                    if (rx_data[8] == '0')
-                    {
-                        return AT_PORTS_DISABLED;
-                    }
-                    else if (rx_data[8] == '1')
-                    {
-                        return AT_PORT_A_SOURCE_C;
-                    }
-                    else if (rx_data[8] == '2')
-                    {
-                        return AT_PORT_A_SOURCE_B;
-                    }
-                    else if (rx_data[8] == '3')
-                    {
-                        return AT_PORT_C_SOURCE_A;
-                    }
-                    else if (rx_data[8] == '4')
-                    {
-                        return AT_PORT_B_SOURCE_A;
-                    }
-                    else if (rx_data[8] == '?')
-                    {
-                        return AT_PORT_NUMBERS_QUERY;
-                    }
-                    else
-                    {
-                        return AT_PORT_CMD_ERROR;
-                    }
-                }
-                else if (rx_data[7] == '?')
-                {
-                    return AT_PORT_SEL;
-                }
-            }
-            // if not followed by "PORT" its unknown
-            else
-            {
-                return AT_UNKNOWN_CMD;
-            }
-        }
-        // if input string starts with "AT" but it is not followed by anything
-        else
-        {
-            return AT_UNKNOWN_CMD;
-        }
+      return AT_UNKNOWN_CMD;
     }
-    // For any other unknown case
-    // Note: Code not never reach here as all cases should be handled above
-    return AT_UNKNOWN_CMD;
+  }
+  // For any other unknown case
+  // Note: Code not never reach here as all cases should be handled above
+  return AT_UNKNOWN_CMD;
 }
 
 void process_input(const char *input)
 {
-    switch (parse_input(input))
+  switch (parse_input(input))
+  {
+  case AT_TEST:
+    printf("\rOK\r\n");
+    break;
+
+  case AT_INFO:
+    printf("Codethink USB C switch\n\rHW:%s\r\nFW:V%s\r\n", HARDWARE_VERSION,
+           FIRMWARE_VERSION);
+    break;
+
+  case AT_PORTS_DISABLED:
+    port_switch_control(PORTS_DISABLED, PORTS_OFF);
+    selected_port = AT_PORTS_DISABLED;
+    printf("\rOK\r\n");
+    break;
+
+  case AT_PORT_A_SOURCE_B:
+    port_switch_control(PORTS_DISABLED, PORTS_OFF);
+    HAL_Delay(DELAY_BW_SWITCH);
+    port_switch_control(PORT_D_EN, PORT_SINK);
+    selected_port = AT_PORT_A_SOURCE_B;
+    printf("\rOK\r\n");
+    break;
+
+  case AT_PORT_A_SOURCE_C:
+    port_switch_control(PORTS_DISABLED, PORTS_OFF);
+    HAL_Delay(DELAY_BW_SWITCH);
+    port_switch_control(PORT_C_EN, PORT_SINK);
+    selected_port = AT_PORT_A_SOURCE_C;
+    printf("\rOK\r\n");
+    break;
+
+  case AT_PORT_B_SOURCE_A:
+    port_switch_control(PORTS_DISABLED, PORTS_OFF);
+    HAL_Delay(DELAY_BW_SWITCH);
+    port_switch_control(PORT_D_EN, PORT_SOURCE);
+    selected_port = AT_PORT_B_SOURCE_A;
+    printf("\rOK\r\n");
+    break;
+
+  case AT_PORT_C_SOURCE_A:
+    port_switch_control(PORTS_DISABLED, PORTS_OFF);
+    HAL_Delay(DELAY_BW_SWITCH);
+    port_switch_control(PORT_C_EN, PORT_SOURCE);
+    selected_port = AT_PORT_C_SOURCE_A;
+    printf("\rOK\r\n");
+    break;
+
+  case AT_PORT_A_B_ON:
+    port_switch_control(PORTS_DISABLED, PORTS_OFF);
+    HAL_Delay(DELAY_BW_SWITCH);
+    port_switch_control(PORT_D_EN, PORT_BI_DIR);
+    selected_port = AT_PORT_A_B_ON;
+    HAL_Delay(DELAY_BW_SWITCH);
+    printf("\rOK\r\n");
+    break;
+
+  case AT_PORT_A_C_ON:
+    port_switch_control(PORTS_DISABLED, PORTS_OFF);
+    HAL_Delay(DELAY_BW_SWITCH);
+    port_switch_control(PORT_C_EN, PORT_BI_DIR);
+    selected_port = AT_PORT_A_C_ON;
+    HAL_Delay(DELAY_BW_SWITCH);
+    printf("\rOK\r\n");
+    break;
+
+  case AT_PORT_A_B_C_ON:
+    port_switch_control(PORTS_DISABLED, PORTS_OFF);
+    HAL_Delay(DELAY_BW_SWITCH);
+    port_switch_control(PORTS_DISABLED, BOTH_PORT_POWER_ON);
+    selected_port = AT_PORT_A_B_C_ON;
+    HAL_Delay(DELAY_BW_SWITCH);
+    printf("\rOK\r\n");
+    break;
+
+  case AT_PORT_SEL:
+    if (selected_port == AT_PORT_A_SOURCE_B)
     {
-    case AT_TEST:
-        printf("\rOK\r\n");
-        break;
-
-    case AT_INFO:
-        printf("Codethink USB C switch\n\rHW:%s\r\nFW:V%s\r\n", HARDWARE_VERSION, FIRMWARE_VERSION);
-        break;
-
-    case AT_PORTS_DISABLED:
-        port_switch_control(PORTS_DISABLED, PORTS_OFF);
-        selected_port = AT_PORTS_DISABLED;
-        printf("\rOK\r\n");
-        break;
-
-    case AT_PORT_A_SOURCE_B:
-        port_switch_control(PORTS_DISABLED, PORTS_OFF);
-        HAL_Delay(DELAY_BW_SWITCH);
-        port_switch_control(PORT_D_EN, PORT_SINK);
-        selected_port = AT_PORT_A_SOURCE_B;
-        printf("\rOK\r\n");
-        break;
-
-    case AT_PORT_A_SOURCE_C:
-        port_switch_control(PORTS_DISABLED, PORTS_OFF);
-        HAL_Delay(DELAY_BW_SWITCH);
-        port_switch_control(PORT_C_EN, PORT_SINK);
-        selected_port = AT_PORT_A_SOURCE_C;
-        printf("\rOK\r\n");
-        break;
-
-    case AT_PORT_B_SOURCE_A:
-        port_switch_control(PORTS_DISABLED, PORTS_OFF);
-        HAL_Delay(DELAY_BW_SWITCH);
-        port_switch_control(PORT_D_EN, PORT_SOURCE);
-        selected_port = AT_PORT_B_SOURCE_A;
-        printf("\rOK\r\n");
-        break;
-
-    case AT_PORT_C_SOURCE_A:
-        port_switch_control(PORTS_DISABLED, PORTS_OFF);
-        HAL_Delay(DELAY_BW_SWITCH);
-        port_switch_control(PORT_C_EN, PORT_SOURCE);
-        selected_port = AT_PORT_C_SOURCE_A;
-        printf("\rOK\r\n");
-        break;
-
-    case AT_PORT_SEL:
-        if (selected_port == AT_PORT_A_SOURCE_B)
-        {
-            printf("\rA source B sink\r\n");
-        }
-        else if (selected_port == AT_PORT_A_SOURCE_C)
-        {
-            printf("\rA source C sink\r\n");
-        }
-        else if (selected_port == AT_PORT_B_SOURCE_A)
-        {
-            printf("\rB source A sink\r\n");
-        }
-        else if (selected_port == AT_PORT_C_SOURCE_A)
-        {
-            printf("\rC source A sink\r\n");
-        }
-        else
-        {
-            printf("\rPORTS DISABLED\r\n");
-        }
-        break;
-
-    case AT_PORT_NUMBERS_QUERY:
-        printf("\r2\r\nOK\r\n");
-        break;
-
-    case AT_PORT_CMD_ERROR:
-        printf("\rERROR: ENTER CORRECT PORT NUMBER\r\n");
-        break;
-
-    case AT_UNKNOWN_CMD:
-        port_switch_control(PORTS_DISABLED, PORTS_OFF);
-        printf("\rERROR: UNKNOWN COMMAND\r\n");
-        break;
-
-    default:
-        break;
+      printf("\rA source B sink\r\n");
     }
+    else if (selected_port == AT_PORT_A_SOURCE_C)
+    {
+      printf("\rA source C sink\r\n");
+    }
+    else if (selected_port == AT_PORT_B_SOURCE_A)
+    {
+      printf("\rB source A sink\r\n");
+    }
+    else if (selected_port == AT_PORT_C_SOURCE_A)
+    {
+      printf("\rC source A sink\r\n");
+    }
+    else if (selected_port == AT_PORT_A_C_ON)
+    {
+      printf("\rBi-Dir A - C\r\n");
+    }
+    else if (selected_port == AT_PORT_A_B_ON)
+    {
+      printf("\rBi-Dir A - B\r\n");
+    }
+    else if (selected_port == AT_PORT_A_B_C_ON)
+    {
+      printf("\rAll ports power on\r\n");
+    }
+    else
+    {
+      printf("\rPORTS DISABLED\r\n");
+    }
+    break;
+
+  case AT_PORT_NUMBERS_QUERY:
+    printf("\r2\r\nOK\r\n");
+    break;
+
+  case AT_PORT_CMD_ERROR:
+    printf("\rERROR: ENTER CORRECT PORT NUMBER\r\n");
+    break;
+
+  case AT_UNKNOWN_CMD:
+    port_switch_control(PORTS_DISABLED, PORTS_OFF);
+    printf("\rERROR: UNKNOWN COMMAND\r\n");
+    break;
+
+  default:
+    break;
+  }
 }
